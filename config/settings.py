c8475b195826fe98ac3eef7eae2816cd36a7a8c5
nazmulhasan7447 <nazmulhasan747406@gmail.com>
2022-04-29 09:37:17 +0600

    updated

--- config/settings.py
+++ config/settings.py
@@ -190,11 +190,11 @@ from .cdn.conf import *
 # email sending set up
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'spinit2win.com@gmail.com'
 EMAIL_HOST_PASSWORD = '1234567Spin'
-EMAIL_PORT = 587
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
