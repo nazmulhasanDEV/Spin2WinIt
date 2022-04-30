a091a8da1038d418536e1640eff8bf48a34fcdf1
nazmulhasan7447 <nazmulhasan747406@gmail.com>
2022-04-30 10:52:21 +0600

    added captcha

--- main.py
+++ main.py
@@ -20,3 +20,15 @@ print(datetime.datetime.now())
 <!--                                    <img src="{% static 'frontEnd/images/banner/3_1.jpg' %}" alt=""/>-->
 <!--                                </a>-->
 <!--                                <a href="https://yllix.com/publishers/444213" target="_blank"><img src="//ylx-aff.advertica-cdn.com/pub/468x60.png" style="width: 100%; height: 205px;border:none;margin:0;padding:0;vertical-align:baseline;" alt="ylliX - Online Advertising Network" /></a>-->
+
+
+
+
+# captcha site key: 6LfA1rEfAAAAAAZPG4Ox-4GOixJt0jwcoCWrZKtv
+# captcha secret key: 6LfA1rEfAAAAAN48KZF86ZooQsLEPW7w1HLOhiIQ
+
+sk:6Lew2rEfAAAAAJhklMS8pgwrna7RIR1ckzdsexvT
+scretk: 6Lew2rEfAAAAAMpaPFyJB12B9Pvxm8fRtn6bONpr
+
+# g_l_sk: 6Ld927EfAAAAAAwVNMGnm5qTAAVybM9WcpFvtPxK
+# g_secret_sk: 6Ld927EfAAAAAJgnfzCtU2uOENl2NU79Vv8V6MuF
