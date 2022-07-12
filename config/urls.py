bbfc8b5e3aa447a431a5813d055a1a8ba0e573e8
nazmulhasan7447 <nazmulhasan747406@gmail.com>
2022-07-12 19:56:47 +0600

    updated

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# updated
 
 
 admin.site.site_header = "Spin & Win Adminstration"
