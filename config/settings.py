3d55867c642890e69291d54eb166f1c3631706ac
nazmulhasan7447 <nazmulhasan747406@gmail.com>
2022-07-21 09:11:34 +0600

    updated product purchase payment page

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import os
 import sys
 import dj_database_url
-import envvar # email setup
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -184,13 +183,13 @@ from .cdn.conf import *
 # email sending set up
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = envvar.email_host
-DEFAULT_FROM_EMAIL = envvar.default_email
-SERVER_EMAIL = envvar.server_email
+EMAIL_HOST = 'smtp.gmail.com'
+DEFAULT_FROM_EMAIL = 'spinit2win.com@gmail.com'
+SERVER_EMAIL = 'spinit2win.com@gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = envvar.host_user
-EMAIL_HOST_PASSWORD = envvar.app_password
+EMAIL_HOST_USER = 'spinit2win.com@gmail.com'
+EMAIL_HOST_PASSWORD = 'mbvnpdclbxjfgmff'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
