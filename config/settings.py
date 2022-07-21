061da7f5460e0ddc0dcab8e69d86d4f747bda1c2
nazmulhasan7447 <nazmulhasan747406@gmail.com>
2022-07-21 09:06:10 +0600

    updated product purchase payment page

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import os
 import sys
 import dj_database_url
-import envvar
+import envvar # email setup
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
